6880a5b33c1960d2c7577e0f6892aff291b77a84
matdev93 <112671782+matdev93@users.noreply.github.com>
2023-07-06 16:38:39 -0400

    ultimos detalles

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const cors = require("cors");
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes')
 
 require('dotenv').config();
 require('./config/database');
 
 const app = express();
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
-
-//* middleware
-//* app.use(userRouter);
-//* app.use(productRouter);
+app.use(userRouter);
+app.use(productRouter);
 
 app.listen(process.env.PORT, () =>
     console.log(`Servidor conectado en puerto: ${process.env.PORT}`)
